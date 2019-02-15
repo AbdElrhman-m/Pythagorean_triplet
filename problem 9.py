8efac5c7cca757a957fb661046151e9e3ea69a1e
AbdElrhman Mohamed <superbido185@gmail.com>
2019-02-15 17:08:09 +0200

    chore: adding a comment to the function
    adding a comment to the function

--- problem 9.py
+++ problem 9.py
@@ -1,14 +1,14 @@
-# superbido 
-# >>>  https://github.com/superbido
-
-
-# a<b<c
-#a*a+b*b = c*c
-# a + b + c = 1000
-# c** 2 = a**2 +b**2
-
+# superbido @bAbdElrhman-m
 
 def Pythagorean_triplet (sum_n):
+	""" 
+	input: sum of 3 numbers as (a,b,c)
+	process:  a < b < c
+		a*a+b*b = c*c
+		a + b + c = 1000
+		c** 2 = a**2 +b**2
+	output: a*b*c
+	"""
 	for a in range(1,sum_n+1):
 		for b in range(1,sum_n+1):
 			if a < b:
@@ -17,6 +17,8 @@ def Pythagorean_triplet (sum_n):
 					print "a: " + str(a) +' b: '+str(b) + " c: " + str(c) 	
 					prod_ans = a * b * c		
 					return "abc = "+ str(prod_ans)
-
+## test code
 print Pythagorean_triplet(12)
+#>>> 60.0
 print Pythagorean_triplet(1000)
+#>>> 31875000.0
